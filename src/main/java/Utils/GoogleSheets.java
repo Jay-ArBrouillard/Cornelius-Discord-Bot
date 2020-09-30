c3a6ea4d1e0ceddb9028e7990fa6049961e491cd
Jay-Ar Brouillard <brouillardjr@gmail.com>
2020-09-29 22:08:36 -0500

    rebuild

--- src/main/java/Utils/GoogleSheets.java
+++ src/main/java/Utils/GoogleSheets.java
@@ -55,7 +55,7 @@ public class GoogleSheets {
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
     private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    private static final String CREDENTIALS_FILE_PATH = "credentials.json";
 
     /**
      * Creates an authorized Credential object.
