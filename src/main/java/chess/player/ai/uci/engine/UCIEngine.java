94ded0505e61d71f59801112e6d2786c3abd1c74
Jay-Ar Brouillard <brouillardjr@gmail.com>
2020-10-20 17:32:22 -0500

    comment prints

--- src/main/java/chess/player/ai/uci/engine/UCIEngine.java
+++ src/main/java/chess/player/ai/uci/engine/UCIEngine.java
@@ -19,7 +19,6 @@ abstract class UCIEngine {
 
             for (Option option : options)
                 passOption(option);
-            waitForReady();
         } catch (IOException e) {
             throw new IOException("Unable to start and bind " + super.getClass().getSimpleName() + " process: ", e);
         }
@@ -35,7 +34,7 @@ abstract class UCIEngine {
     }
 
     void sendCommand(String command) throws IOException {
-        System.out.println("sendCommand:"+command); //To remove
+//        System.out.println("sendCommand:"+command); //To remove
         output.write(command + "\n");
         output.flush();
     }
@@ -44,7 +43,7 @@ abstract class UCIEngine {
         String line;
 
         while ((line = input.readLine()) != null) {
-            System.out.println("readLine:"+line); //To remove
+//            System.out.println("readLine:"+line); //To remove
             if (line.startsWith(expected))
                 return line;
         }
@@ -57,7 +56,7 @@ abstract class UCIEngine {
         String line;
 
         while ((line = input.readLine()) != null) {
-            System.out.println("readResponse:"+line); //To remove
+//            System.out.println("readResponse:"+line); //To remove
             lines.add(line);
 
             if (line.startsWith(expected) || line.startsWith("Protocol not found")) //In some UCI isReady is not needed
