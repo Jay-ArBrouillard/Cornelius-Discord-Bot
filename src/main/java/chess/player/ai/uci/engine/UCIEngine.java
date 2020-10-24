2c3cbc6a28aa224a7965b8b0097dad18346cbdad
Jay-Ar Brouillard <brouillardjr@gmail.com>
2020-10-24 15:01:35 -0500

    turn debug back on for uciengine

--- src/main/java/chess/player/ai/uci/engine/UCIEngine.java
+++ src/main/java/chess/player/ai/uci/engine/UCIEngine.java
@@ -69,7 +69,7 @@ abstract class UCIEngine {
     }
 
     void sendCommand(String command) throws IOException {
-//        System.out.println("sendCommand:"+command); //To remove
+        System.out.println("sendCommand:"+command); //To remove
         output.write(command + "\n");
         output.flush();
     }
@@ -78,7 +78,7 @@ abstract class UCIEngine {
         String line;
 
         while ((line = input.readLine()) != null) {
-//            System.out.println("readLine:"+line); //To remove
+            System.out.println("readLine:"+line); //To remove
             if (line.startsWith(expected))
                 return line;
         }
@@ -91,7 +91,7 @@ abstract class UCIEngine {
         String line;
 
         while ((line = input.readLine()) != null) {
-//            System.out.println("readResponse:"+line); //To remove
+            System.out.println("readResponse:"+line); //To remove
             lines.add(line);
 
             if (line.startsWith(expected) || line.startsWith("Protocol not found")) //In some UCI isReady is not needed
