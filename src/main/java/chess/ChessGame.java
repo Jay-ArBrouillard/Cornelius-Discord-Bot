d23401eec9c627cbe6cb1cf684617150efb63776
Jay-Ar Brouillard <brouillardjr@gmail.com>
2020-10-31 10:09:12 -0500

    Added .txt at end for Rodent

--- src/main/java/chess/ChessGame.java
+++ src/main/java/chess/ChessGame.java
@@ -195,7 +195,7 @@ public class ChessGame {
             setClient(new RodentClient.Builder()
                     //Rodent passes personality. One of the personalities called "Simple" which will be called Rodent instead
                     //otherwise we use the name field
-                    .setOption(Option.Personality, name.length == 1 ? name[0].toLowerCase() : name[1].toLowerCase())
+                    .setOption(Option.Personality, name.length == 1 ? name[0].toLowerCase()+".txt" : name[1].toLowerCase()+".txt")
                     .build(), p);
         }
         else if (p.name.contains("Randy Random")) {
