abd6e283e38fc622699a5de7f3e6b1e20581ce71
Jay-Ar Brouillard <brouillardjr@gmail.com>
2020-11-06 19:20:59 -0600

    Removed Error.equals(status) from gameOver var

--- src/main/java/commands/ChessCommand.java
+++ src/main/java/commands/ChessCommand.java
@@ -674,7 +674,7 @@ public class ChessCommand {
                 reply = state.getMessage();
                 status = state.getStatus();
 
-                boolean isGameOver = CHECKMATE.equals(status) || DRAW.equals(status) || COMPUTER_RESIGN.equals(status) || ERROR.equals(status);
+                boolean isGameOver = CHECKMATE.equals(status) || DRAW.equals(status) || COMPUTER_RESIGN.equals(status);
                 long minutesElapsed = (Instant.now().toEpochMilli() - state.getMatchStartTime()) / 1000 / 60;
                 if (minutesElapsed >= 3.5) { //3.5 minutes
                     if (chessGame.didWhiteJustMove()) {
