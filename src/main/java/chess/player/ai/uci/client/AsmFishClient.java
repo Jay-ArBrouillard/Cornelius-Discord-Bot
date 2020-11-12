309288f5dac5706931b4dc9acad8c9afd73c480e
Jay-Ar Brouillard <brouillardjr@gmail.com>
2020-11-11 19:48:23 -0600

    Change asmFish version

--- src/main/java/chess/player/ai/uci/client/AsmFishClient.java
+++ src/main/java/chess/player/ai/uci/client/AsmFishClient.java
@@ -43,7 +43,7 @@ public class AsmFishClient extends BaseAiClient{
     public static class Builder {
         private Set<Option> options = new HashSet<>();
         private Variant variant = Variant.DEFAULT;
-        private String path = "bin/asmFishL_9_base";
+        private String path = "bin/asmFishL_9_bmi2";
 
         public final AsmFishClient.Builder setVariant(Variant v) {
             variant = v;
